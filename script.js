32265fc76db587161ce8948d5c088eaaeb07766e
Oliwer Kirkiewicz <oliwer.kirkiewicz@gmail.com>
2023-07-23 14:36:49 +0200

    impovements
    added 'use strict'

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+'use strict'
 const body = document.querySelector('body')
 const localDataBase = [];
 let pokemonsFetchesSoFar = 0;
 let currentPage = 0;
+const cardWrapper = document.querySelector('.cardWrapper')
+
 
 async function fetchPokemons(numOfFetches) {
     const wrapper = document.querySelector('.wrapper')
@@ -26,34 +29,44 @@ async function fetchPokemons(numOfFetches) {
     }
 }
 
-async function domUpdate(callback, direction) {
+async function domUpdate(direction) {
+ 
     if (direction === 'next') {
         currentPage++;
-    } else if (direction === 'previous' && currentPage > 1) {
+    } 
+    else if (typeof direction === 'number'){
+currentPage = direction;
+    }
+    else if (direction === 'previous' && currentPage > 1) {
         currentPage--;
     } else {
         return;
     }
 
     if (currentPage >= localDataBase.length) {
-        await callback(3);
+        const loadingKontent = '<div class="loadingArm"><div class="loadingLine"></div></div>'
+        const loading = createHtmlEl('div', 'loading', loadingKontent, undefined)
+        cardWrapper.innerHTML ='';
+        cardWrapper.appendChild(loading);
+       
+        
+        await fetchPokemons(3);
     }
 
-    createMain();
+    imgsUpdate();
 
     createNav();
 }
 
-function createMain() {
-    const cardWrapper = document.querySelector('.cardWrapper')
-
+function imgsUpdate() {
     const currentArrayOfPokemons = localDataBase[currentPage - 1]
+
     const getPokemonImg = (indx) => currentArrayOfPokemons[indx].sprites.other.dream_world.front_default
     const getPokemonName = (indx) => currentArrayOfPokemons[indx].name.toUpperCase()
     cardWrapper.innerHTML = '';
 
     currentArrayOfPokemons.forEach((_, i) => {
-        const cardinnerHTML = `<img src="${getPokemonImg(i)}">
+        const cardinnerHTML = `<img src="${getPokemonImg(i)}" alt="${getPokemonName(i)}">
         <div class="shineEffect"/></div>
         <div class="imgOverlay"/></div>
         <div class="name">${getPokemonName(i)}</div>`
@@ -62,7 +75,7 @@ function createMain() {
     })
 }
 
-function createNav() {
+async function createNav() {
     const currentPageNum = document.querySelector('.numOfActivePageInNav');
     currentPageNum.innerHTML = currentPage
     const restNavEl = createHtmlEl('div', 'restNav')
@@ -71,29 +84,35 @@ function createNav() {
     for (let i = 0; i < localDataBase.length; i++) {
         const singeNumInsideNav = createHtmlEl('span', `num${(i+1)}`, i + 1)
         restNavEl.appendChild(singeNumInsideNav)
+        const navNumbers = Array.from(document.querySelectorAll('.restNav span'))
+
+    navNumbers.forEach(number => {
+        number.addEventListener('click', async () => domUpdate((Number(number.innerHTML))))
+    })
     }
 }
 
 async function eventListeners(){
     const next = document.querySelector('.next')
     const previous = document.querySelector('.previous')
+  
     next.addEventListener('click', async () => {
-        await domUpdate(fetchPokemons, 'next');
+        await domUpdate('next');
     });
 
     previous.addEventListener('click', async () => {
-        await domUpdate(fetchPokemons, 'previous');
+        await domUpdate('previous');
     });
     window.addEventListener('keydown', async ({key}) => {
         if (key === 'ArrowRight') {
-            await domUpdate(fetchPokemons, 'next')
+            await domUpdate('next')
         } else if (key === 'ArrowLeft') {
-            await domUpdate(fetchPokemons, 'previous')
+            await domUpdate('previous')
         }
     })
-}
+    }
 
-function hoverEffects() {
+function hoverEffect() {
     const pokeBall = document.querySelector('.pokeBall')
     const cardWrapper = document.querySelector('.cardWrapper')
     pokeBall.addEventListener('mouseover', () => {
@@ -120,12 +139,11 @@ function createHtmlEl(type, klasa, content, source) {
             element.src = source
         }
     }
-
     return element
 }
 
 (async () => {
-    await domUpdate(fetchPokemons, 'next');
-    hoverEffects();
+    await domUpdate('next');
+    hoverEffect();
     eventListeners();
 })();
